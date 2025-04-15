09f18de10e5b45179d56abb5daf93e80ce4abf51
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-04-16 00:50:58 +0300

    Refactor main.ts to use a separate Pinia instance with persisted state and moodo instance provision

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import moodoInstance from './moodo/moodo'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.provide('moodo', moodoInstance())
 
 app.mount('#app')
