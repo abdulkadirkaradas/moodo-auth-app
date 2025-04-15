e896950fcd66de10c165e3afb174690ea87023cc
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-04-16 00:48:58 +0300

    Add IndexPage component

---
+++ src/components/IndexPage.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<template>
+  <div class="homepage">
+    <h1>Welcome to Homepage!</h1>
+    <p>This is the homepage component for the Moodo Auth App.</p>
+  </div>
+</template>
+
+<style scoped>
+.homepage {
+  text-align: center;
+}
+
+h1 {
+  font-size: 2.5rem;
+  color: #ffffff;
+}
+
+p {
+  font-size: 1.2rem;
+  color: #ffffff;
+}
+</style>
