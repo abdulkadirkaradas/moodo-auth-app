ce6292ac937237d6811d6b7c3a81a1395415c216
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-04-16 00:48:42 +0300

    Add GuestPage component

---
+++ src/components/GuestPage.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  name: "GuestPage",
+};
+</script>
+
+<template>
+  <div class="homepage">
+    <h1>Welcome to Guest Page!</h1>
+    <p>This is the welcome component for the Moodo Auth App.</p>
+  </div>
+</template>
+
+<style scoped>
+.homepage {
+  text-align: center;
+}
+
+h1 {
+  font-size: 2.5rem;
+  color: #ffffff;
+}
+
+p {
+  font-size: 1.2rem;
+  color: #ffffff;
+}
+</style>
